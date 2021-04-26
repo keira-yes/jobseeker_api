bb7c00074f12bc2fdd425229af88051ff0613428
Irina Kirillova <keira.kirillova@gmail.com>
2021-04-26 20:34:30 +0300

    add mongoose, connection to mongodb

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const companies = require('./routes/companies');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+const companies = require('./routes/companies');
 
-dotenv.config({ path: './config/.env' });
+dotenv.config({ path: './config/config.env' });
+
+// Connect to mongodb
+connectDB();
 
 const app = express();
 
+// Add logger to requests
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Routes
 app.use('/api/v1/companies', companies);
 
 const PORT = process.env.PORT || 5000;
 const {NODE_ENV} = process.env;
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT} port in ${NODE_ENV} mode`));
+const server = app.listen(PORT, () => console.log(`Server is running on ${PORT} port in ${NODE_ENV} mode`));
+
+// Handle error of connection to server
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+})
