57290122da47c506edaf3ea858ffd74e96144542
Irina Kirillova <keira.kirillova@gmail.com>
2021-05-03 22:11:34 +0300

    handle mongoose errors of bad id, fields duplication and validation

--- middleware/error.js
+++ middleware/error.js
@@ -4,11 +4,24 @@ const errorHandler = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
-    console.log(err.stack.brightRed);
+    console.log(err);
 
+    // Mongoose error - ObjectId failed
     if (err.name === 'CastError') {
         const message = `Company with id ${err.value} not found`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose error - Duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value was found';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose error - Validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message)
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
