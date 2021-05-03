31ae5d6188f62ee78ed04b5d74dbdba63f413c98
Irina Kirillova <keira.kirillova@gmail.com>
2021-05-03 22:38:34 +0300

    add company slug with slugify package and using document pre middleware

--- models/Company.js
+++ models/Company.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const CompanySchema = new mongoose.Schema({
    name: {
@@ -86,5 +87,11 @@ const CompanySchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+// Create company slug
+CompanySchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
 
 module.exports = mongoose.model('Company', CompanySchema);
